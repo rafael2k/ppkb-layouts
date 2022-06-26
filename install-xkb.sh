5e7abeba6bd237d89fc4f1d4221036692fef06b8
Phalio <phalio@disroot.org>
2022-06-26 15:18:04 +0200

    xkb: added more evdev files because apparently one is not enough

--- install-xkb.sh
+++ install-xkb.sh
@@ -4,3 +4,6 @@ echo "Copying files:"
 cp -v xkb/pp /usr/share/X11/xkb/symbols/
 cp -v xkb/pp-usrspc /usr/share/X11/xkb/symbols/
 cp -v xkb/evdev.xml /usr/share/X11/xkb/rules/
+cp -v xkb/evdev.xml /usr/share/X11/xkb/rules/base.xml
+cp -v xkb/evdev.lst /usr/share/X11/xkb/rules/
+cp -v xkb/evdev.lst /usr/share/X11/xkb/rules/base.lst
