8f486fc2faa8c639cdd05b5ff0b859ebae328d9f
phalio <phalio@disroot.org>
2022-08-05 20:24:53 +0200

    renamed usrspc to driver since it now refers to usrspc and kernel d.

--- install-xkb.sh
+++ install-xkb.sh
@@ -2,7 +2,7 @@
 
 echo "Copying files:"
 cp -v xkb/pp /usr/share/X11/xkb/symbols/
-cp -v xkb/pp-usrspc /usr/share/X11/xkb/symbols/
+cp -v xkb/pp-driver /usr/share/X11/xkb/symbols/
 cp -v xkb/evdev.xml /usr/share/X11/xkb/rules/
 cp -v xkb/evdev.xml /usr/share/X11/xkb/rules/base.xml
 cp -v xkb/evdev.lst /usr/share/X11/xkb/rules/
